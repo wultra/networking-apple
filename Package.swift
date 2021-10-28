0e0a08150bb3fb8ceeec276126814f0f285f0f7f
Juraj Ďurech <durech.juraj@gmail.com>
2021-10-28 15:44:09 +0200

    Fix #10: Added support for Swift Package Manager

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.4
+
+import PackageDescription
+
+let package = Package(
+    name: "WultraPowerAuthNetworking",
+    platforms: [
+        .iOS(.v10),
+        .tvOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "WultraPowerAuthNetworking",
+            targets: ["WultraPowerAuthNetworking"]),
+    ],
+    dependencies: [
+        .package(name: "PowerAuth2", url: "https://github.com/wultra/powerauth-mobile-sdk-spm.git", .branch("develop")),
+    ],
+    targets: [
+        .target(
+            name: "WultraPowerAuthNetworking",
+            dependencies: ["PowerAuth2", .product(name: "PowerAuthCore", package: "PowerAuth2")])
+    ]
+)
