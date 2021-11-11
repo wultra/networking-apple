8a130a716e771068d5d29a2cae84d64e217f10d3
Juraj Ďurech <durech.juraj@gmail.com>
2021-11-11 13:10:50 +0100

    Rollback to carthage

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,13 +5,58 @@ set -u # stop when undefined variable is used
 #set -x # print all execution (good for debugging)
 
 SCRIPT_FOLDER=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+"$SCRIPT_FOLDER/cart-update.sh"
 pushd "${SCRIPT_FOLDER}/.."
 
+echo "---------------------------------------------------"
+echo "iOS"
+echo "---------------------------------------------------"
 xcrun xcodebuild build \
     -project "WultraPowerAuthNetworking.xcodeproj" \
     -scheme "WultraPowerAuthNetworking" \
     -configuration "Release" \
+    -destination "generic/platform=iOS" \
     CODE_SIGN_IDENTITY="" \
     CODE_SIGNING_REQUIRED=NO
-
+echo "---------------------------------------------------"
+echo "iOS Simulator"
+echo "---------------------------------------------------"
+xcrun xcodebuild build \
+    -project "WultraPowerAuthNetworking.xcodeproj" \
+    -scheme "WultraPowerAuthNetworking" \
+    -configuration "Release" \
+    -destination "generic/platform=iOS Simulator" \
+    CODE_SIGN_IDENTITY="" \
+    CODE_SIGNING_REQUIRED=NO
+echo "---------------------------------------------------"
+echo "mac Catalyst"
+echo "---------------------------------------------------"
+xcrun xcodebuild build \
+    -project "WultraPowerAuthNetworking.xcodeproj" \
+    -scheme "WultraPowerAuthNetworking" \
+    -configuration "Release" \
+    -destination "platform=macOS,variant=Mac Catalyst" \
+    SUPPORTS_MACCATALYST=YES \
+    CODE_SIGN_IDENTITY="" \
+    CODE_SIGNING_REQUIRED=NO
+echo "---------------------------------------------------"
+echo "tvOS"
+echo "---------------------------------------------------"
+xcrun xcodebuild build \
+    -project "WultraPowerAuthNetworking.xcodeproj" \
+    -scheme "WultraPowerAuthNetworking" \
+    -configuration "Release" \
+    -destination "generic/platform=tvOS" \
+    CODE_SIGN_IDENTITY="" \
+    CODE_SIGNING_REQUIRED=NO
+echo "---------------------------------------------------"
+echo "tvOS Simulator"
+echo "---------------------------------------------------"
+xcrun xcodebuild build \
+    -project "WultraPowerAuthNetworking.xcodeproj" \
+    -scheme "WultraPowerAuthNetworking" \
+    -configuration "Release" \
+    -destination "generic/platform=tvOS Simulator" \
+    CODE_SIGN_IDENTITY="" \
+    CODE_SIGNING_REQUIRED=NO
 popd
