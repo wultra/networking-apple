cc33dfa16f538a60594e65291813b5e52655d6e3
Marek Stransky <stranskymarek90@gmail.com>
2025-01-27 14:27:26 +0100

    Fix build script - update macOs and Xcode version

--- scripts/xcodeselect.sh
+++ scripts/xcodeselect.sh
@@ -3,6 +3,6 @@
 set -e
 
 # Script used for selecting proper xcode for all builds on the CI (not appcenter).
-# Available xcodes at https://github.com/actions/runner-images/blob/main/images/macos/macos-14-Readme.md#xcode
+# Available xcodes at https://github.com/actions/runner-images/blob/main/images/macos/macos-15-Readme.md#xcode
 
-sudo xcode-select -s "/Applications/Xcode_16.app"
+sudo xcode-select -s "/Applications/Xcode_16.2.app"
