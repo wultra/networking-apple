be7f030d97488f2facf181c85e064ff50d6423e8
Jan Kobersky <kober32@gmail.com>
2025-04-03 12:54:11 +0200

    Fixed test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,8 +6,13 @@ set -u # stop when undefined variable is used
 
 SCRIPT_FOLDER=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+# find latest iOS SDK available
 IOS_VERSION=$(xcrun simctl list | grep "\-\- iOS" | tail -1 | tr -d - | tr -d " " | tr -d "iOS")
-DESTINATION="platform=iOS Simulator,OS=${IOS_VERSION},name=iPhone 16"
+# find the first simulator for this sdk
+SIMULATOR=$(xcrun simctl list | grep "\-\- iOS ${IOS_VERSION} \-\-" -A 1 | tail -1 | sed -E 's/^[[:space:]]+//; s/\(.*//; s/[[:space:]]+$//')
+DESTINATION="platform=iOS Simulator,OS=${IOS_VERSION},name=${SIMULATOR}"
+
+echo "Destination resolved: ${DESTINATION}"
 
 pushd "${SCRIPT_FOLDER}"
 sh cart-update.sh
