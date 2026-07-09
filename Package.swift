614b45d523cf83a596d9566b288a4c8d98bd52b0
Jan Kobersky <kober32@gmail.com>
2026-07-09 13:20:24 +0200

    Pointing to PA mtoken-demo/qa-builds in SPM

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["WultraPowerAuthNetworking"])
     ],
     dependencies: [
-        .package(url: "https://github.com/wultra/powerauth-mobile-sdk.git", branch: "develop")
+        .package(url: "https://github.com/wultra/powerauth-mobile-sdk.git", branch: "mtoken-demo/qa-builds")
     ],
     targets: [
         .target(
