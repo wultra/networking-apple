3142a79a0af6a1ac91a6227054bf548cfc12af26
Marek Chrenko <marek.chrenko@wultra.com>
2026-07-15 14:00:22 +0200

    change PA SDK dependency to 2.0.0-RC1

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["WultraPowerAuthNetworking"])
     ],
     dependencies: [
-        .package(url: "https://github.com/wultra/powerauth-mobile-sdk.git", branch: "develop")
+        .package(url: "https://github.com/wultra/powerauth-mobile-sdk.git", revision: "2.0.0-RC1")
     ],
     targets: [
         .target(
